ec8cd1293c91fd55fc00085c3485435b2b51fed3
md hasnahin sehan <92535312+shehanaip@users.noreply.github.com>
2026-05-03 21:57:40 +0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+const path = require("path");
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -116,8 +118,10 @@ app.post("/api/create-checkout-session", async (req, res) => {
       ],
 
       success_url:
-        "https://masstech-react-1.onrender.com/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "https://masstech-react-1.onrender.com/store"
+        "https://masstech-react.onrender.com/success?session_id={CHECKOUT_SESSION_ID}",
+
+      cancel_url:
+        "https://masstech-react.onrender.com/store"
     });
 
     console.log("✅ SESSION CREATED:", session.id);
@@ -174,15 +178,19 @@ app.get("/api/admin/purchases", adminAuth, async (req, res) => {
 });
 
 /* =========================
-   TEST
+   SERVE REACT BUILD
 ========================= */
-app.get("/", (req, res) => {
-  res.send("");
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 /* =========================
    SERVER
 ========================= */
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
